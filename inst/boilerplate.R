5c940419ae23e276dc946745d006d6f9f3fc76af
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2026-04-25 13:13:05 +0200

    adding new datapackage

--- inst/boilerplate.R
+++ inst/boilerplate.R
@@ -7,29 +7,53 @@ library(opentimeseries)
 # outcommented because by the time you read this in boilerplate.R
 # you've already created the archive.
 # archive_init("ch.kof.globalbaro", parent_dir = )
-
-
-
-## Example Step 2, Generate History
-
 library(kofdata)
 library(data.table)
 library(tsbox)
 
-global <- get_collection("globalbaro_vintages")
-names(global) <- gsub("globalbaro_","",names(global))
-names(global) <- sub("_", "\\.", names(global))
-class(global) <- c(class(global), "tslist")
-release_dates <- rep(seq(as.Date("2020-01-10"),
-                         by = "1 month",
-                         length.out = length(global)/2),2)
-vintages_dt <- create_vintage_dt(release_dates, global)
+baro <- kofdata::get_collection("baro_vintages_monthly")
+# change this to index to represent new metadata changes
+names(baro) <- rep("idx", length(baro))
+class(baro) <- c(class(baro), "tslist")
+release_dates <- rep(seq(as.Date("2014-04-10"),
+  by = "1 month",
+  length.out = length(baro)
+), 2)
+
+vintages_dt <- create_vintage_dt(release_dates, baro)
 head(vintages_dt)
 
 ## Example Step 3, Import History to Archive
+# getwd() prints NULL ?
+setwd("~/KOF_Lab/opentsi/ch.kof.barometer")
 archive_import_history(vintages_dt, repository_path = ".")
 
 
+# write process data done (vignette 2)
+# done
+
+# write metadata (vignette 3)
+# done
+library(deloRean)
+render_metadata()
+meta <- read_meta(".")
+validate_metadata(meta) # TRUE
+
+devtools::load_all()
+devtools::check()
+devtools::install()
+
+# finish archive_seal
+checksum_input <- generate_checksum_input()
+archive_seal(checksum_input)
+
+# check handle data and process data
+library(digest)
+library(kofdata)
+handle_update()
+ 
+# automation
+# done
 
 
 
